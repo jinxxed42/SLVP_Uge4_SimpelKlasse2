7ec830729a9cd5179c88b149d7b2abf132eba777
Claus Damgaard <jinxxed@outlook.com>
2023-03-22 16:25:00 +0100

    Add project files.

---
+++ Beer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SLVP_Uge4_SimpelKlasse2
+{ 
+    internal class Beer
+    {
+        public string name;
+        public int size;
+        public int price;
+        public Boolean alcohol;
+        public double procent;
+
+        public Beer(string name, int size, int price, Boolean alcohol, double procent)
+        {
+            this.name = name;
+            this.size = size;
+            this.price = price;
+            this.alcohol = alcohol;
+            this.procent = procent;
+        }
+
+        public string ToString()
+        {
+            return "Navn: " + name + " Størrelse: " + size + "cL. Indeholder alkohol: " + (alcohol ? "ja" : "nej") + ". Procent: " + procent + "%.";
+        }
+
+        public int GetPrice()
+        {
+            return price;
+        }
+
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using SLVP_Uge4_SimpelKlasse2;
+
+List<Beer> beerList = new List<Beer>();
+
+Boolean oneMore;
+do
+{
+    Console.WriteLine("Indtast type øl: ");
+    string beerType = Console.ReadLine();
+    while (beerType == "")
+    {
+        Console.WriteLine("Du skal indtaste noget tekst. Prøv igen.");
+        beerType = Console.ReadLine();
+    }
+    Console.WriteLine("Indtast et heltal for størrelse i cL: ");
+    int beerSize;
+    while (!int.TryParse(Console.ReadLine(), out beerSize) || beerSize < 0)
+    {
+        Console.WriteLine("Ugyldig værdi. Prøv igen.");
+    }
+    Console.WriteLine("Indtast et heltal for prisen i kr: ");
+    int beerPrice;
+    while (!int.TryParse(Console.ReadLine(), out beerPrice) || beerPrice < 0)
+    {
+        Console.WriteLine("Ugyldig værdi. Prøv igen.");
+    }
+    Console.WriteLine("Indeholder den alkohol? - Ja eller nej: ");
+    Boolean beerAlcohol;
+    string svar = Console.ReadLine();
+    while (svar.ToLower() != "ja" && svar.ToLower() != "nej")
+    {
+        Console.WriteLine("Prøv igen - Ja eller nej:");
+        svar = Console.ReadLine();
+    }
+    if (svar == "ja")
+    {
+        beerAlcohol = true;
+    }
+    else
+    {
+        beerAlcohol = false;
+    }
+    double beerProcent;
+    if (beerAlcohol) 
+    { 
+        Console.WriteLine("Indtast dens procent: ");
+        while (!double.TryParse(Console.ReadLine(), out beerProcent) || beerProcent < 0 || beerProcent > 100)
+        {
+            Console.WriteLine("Ugyldig værdi. Prøv igen.");
+        }
+    }
+    else
+    {
+        beerProcent = 0;
+    }
+    Console.WriteLine("Skal du have mere? - Ja eller nej: ");
+    svar = Console.ReadLine();
+    while (svar.ToLower() != "ja" && svar.ToLower() != "nej")
+    {
+        Console.WriteLine("Prøv igen - Ja eller nej:");
+        svar = Console.ReadLine();
+    }
+    if (svar == "ja")
+    {
+        oneMore = true;
+    }
+    else
+    {
+        oneMore = false;
+    }
+    Beer beer = new Beer(beerType, beerSize, beerPrice, beerAlcohol, beerProcent);
+    if (beer is not null) beerList.Add(beer);
+} while (oneMore);
+
+foreach (Beer b in beerList)
+{
+    Console.Write(b.ToString());
+    Console.WriteLine(" Prisen er: " + b.GetPrice() + "kr.");
+}
